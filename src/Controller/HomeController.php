ca6e5152dd881c49ad6efaf6e99fd17ac17ebff2
Miquel Castro <castro-miquel@hotmail.com>
2023-10-28 15:23:32 +0200

    Add search by name

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Characters;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
 
     #[Route('/')]
-    public function homepage(EntityManagerInterface $entityManager)
+    public function homepage(EntityManagerInterface $entityManager, Request $request)
     {
-        $characters =  $entityManager->getRepository(Characters::class)->findAll();
+        $search = $request->query->get('search');
 
+        if ($search) {
+            $characters = $entityManager->getRepository(Characters::class)->findBy(['name' => $search]);
+        } else {
+            $characters = $entityManager->getRepository(Characters::class)->findAll();
+        }
         return $this->render('character.html.twig', [
             'characters' => $characters,
         ]);
